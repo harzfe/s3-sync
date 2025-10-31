13544ee60fb1d70caca0ae16e27a0f3e958bf0ab
harzfe <felixharz99@gmail.com>
2025-10-31 20:41:50 +0100

    setup base project

---
+++ src/main/java/com/example/s3_sync/S3SyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.s3_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3SyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3SyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/s3_sync/S3SyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s3_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3SyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
